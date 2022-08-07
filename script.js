097b96f5fcaccafc9e0fa17380e032d400760353
Piotr Ogrodnik <110233868+cytrusowykrzak@users.noreply.github.com>
2022-08-07 19:34:44 +0200

    Added registration page

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-const container=document.querySelector(".container");
+const signup=document.querySelector(".signuptext");
+const login=document.querySelector(".logintext");
+const loginpage=document.querySelector(".forms");
+const registerpage=document.querySelector(".form");
+const email=document.querySelector("email");
+const password=document.querySelector("password");
+const submit=document.querySelector("submit");
+const form=document.querySelector("form");
+
+//
+const hidder = ()=>{
+
+    loginpage.style.display="none";
+    registerpage.style.display='';
+    registerpage.style.transition = "all 2s";
+
+};
+const showpage=()=>{
+    loginpage.style.display='';
+    registerpage.style.display="none";
+}
+
+/*form.addEventListener('submit',(e)=>{
+    let messages=[]
+    if(email.value===''|| email.value===null)
+    {
+        messages.push('name is required')
+}
+    if(password.value.lenght<=6){
+        messages.push('Password must be longer than 6 characters')
+    }
+
+    if(messages.length>0){
+        e.preventDefault()
+    }
+})*/
+
+
+
+signup.addEventListener("click",hidder);
+login.addEventListener("click",showpage);
+
+
